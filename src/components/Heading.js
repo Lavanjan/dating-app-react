9f815edf99212beb81895fdbf0bcc97eab704e57
lavanjan <ravilavanjan23@gmail.com>
2020-04-20 19:11:04 +0530

    Import Header Part

---
+++ src/components/Heading.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Heading extends Component {
+    render() {
+        return (
+            <div className="home">
+                <div className = "homeLogo">
+                <h3>Date</h3>
+                </div>
+                <form>
+                    <input type = "text" placeholder = "search" value = "search"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Heading
